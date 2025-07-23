06e2cd7f66ea8973cc23f31b5d15d235a2635b1f
Anish13579 <anish.sarkar@hyland.com>
2025-07-23 04:02:34 -0400

    Create Documentation for Final_Code.py

---
+++ Final/Documentation for Final_Code.py
@@ -0,0 +1,7 @@
+Install the following Python libraries before executing the "Final_Code.py" file :-
+  1. numpy 
+  2. scikit-learn
+
+  Use this command for installing the above libraries: "pip install scikit-learn"
+
+When using the "Final_Code.py" file, change the variable "csv_file_path" to the relative path of the desired csv file needed for the grouping of the processes.
